31f7990637d783dda3f5eeec0363203d94a31c4e
ShirvanyanKaren <kshirvanyan2000@gmail.com>
2023-12-22 05:56:05 -0800

    updated ssl

--- server/config/connection.js
+++ server/config/connection.js
@@ -6,21 +6,31 @@ const Sequelize = require('sequelize');
 dotenv.config();
 
 const sequelize = process.env.DATABASE_URL
-    ? new Sequelize(process.env.DATABASE_URL)
+    ? new Sequelize(process.env.DATABASE_URL, {
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    })
     : new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-        host: 'localhost',
-        dialect: 'postgres',
-        port: 5500,
-    }
-);
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PW,
+        {
+            host: 'localhost',
+            dialect: 'postgres',
+            port: 5500,
+        }
+    );
 
 module.exports = sequelize;
 
 
 
+// postgres://bldnzmviigksrq:332889e3062e2302bf458bc3d07040b9b12888b447422e5cf41e9ca415bfcc15@ec2-44-206-204-65.compute-1.amazonaws.com:5432/dcao9skcfluvkl
 
+// psql --host=ec2-44-206-204-65.compute-1.amazonaws.com --port=5432 --username=bldnzmviigksrq --password --dbname=dcao9skcfluvkl
 
+// 332889e3062e2302bf458bc3d07040b9b12888b447422e5cf41e9ca415bfcc15
