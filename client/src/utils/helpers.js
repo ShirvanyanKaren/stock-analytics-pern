0646b8c7c3f43e03b90a825778568e70d7661f6a
ShirvanyanKaren <kshirvanyan2000@gmail.com>
2023-12-28 04:37:41 -0800

    dockerized back end works, and now deployed

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const pyBackEnd = typeof process == 'object' ? process.env.BACK : "http://0.0.0.0:8000";
-// const pyBackEnd = process.env.BACK ? process.env.BACK : "https://pern-portfolio-backend-805cd64a428d.herokuapp.com"
+// const pyBackEnd = typeof process == 'object' ? process.env.BACK : "http://0.0.0.0:8000";
+const pyBackEnd = process.env.BACK ? process.env.BACK : "https://pern-portfolio-backend-805cd64a428d.herokuapp.com"
 
 export const indexOptions = {
     "SP500": "^GSPC",
